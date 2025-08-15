a640a5c23b54cfd329f31724bd776a8b2385f349
prathamshettyy <prathamr.cs22@sahyadri.edu.in>
2025-08-15 17:47:19 +0530

    refactor: remove user query and update to send expiry alerts in check_users.py

--- check_users.py
+++ check_users.py
@@ -1,7 +1,8 @@
-from app import app, db, User 
+# check_users.py
+from app import app, send_expiry_alerts
 
-
-with app.app_context():
-    users = User.query.all()  
-    for user in users:
-        print(user.username, user.email)  
+if __name__ == '__main__':
+    # The app_context is needed so the script can access the database
+    # and other parts of your Flask application.
+    with app.app_context():
+        send_expiry_alerts()
